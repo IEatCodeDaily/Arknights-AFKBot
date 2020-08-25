5d508664dec9bef4b23be3a6c1a0e84167839a15
IEatCodesDaily <60133189+IEatCodesDaily@users.noreply.github.com>
2020-08-25 10:15:04 +0700

    active window checker initial implementation

---
+++ getprocess.py
@@ -0,0 +1,17 @@
+from win32 import win32gui
+import pywinauto
+
+open_windows = [[]]
+
+#print(active, win32gui.GetWindowText(active))
+def getactive():
+    awin32gui.GetForegroundWindow()
+
+def getprocess(hwnd, var):
+    if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowTextLength(hwnd) != 0:
+            open_windows.append([hwnd,win32gui.GetWindowText(hwnd)])
+
+
+win32gui.EnumWindows(getprocess, None)
+print(open_windows)
+print(win32gui.GetForegroundWindow())
--- main.py
+++ main.py
@@ -1,49 +1,51 @@
 import pyautogui
 import time
-from random import random
-
-def randomfloat(min,max):
-    return min + random()*(max - min)
+import getprocess
 
 
-images = ["Image/Stage_startb.png","Image/Mission_start.png","Image/Stage_finish.png"]
-count = 0
-stage_started = 0
-mission_started = 0
-mission_finished = 0
+def main():
+    images = ["Image/Stage_startb.png","Image/Mission_start.png","Image/Stage_finish.png"]
+    count = 0
+    stage_started = False
+    mission_started = False
+    mission_finished = False
+    correct_windows = True
+    while True:
+        
+        while correct_windows == True:
+            print("\rChecking for %s" %images[0])
+            while stage_started == 0:
+                try:
+                    count += 1
+                    if count == 3:
+                        pyautogui.click(1700,300)
+                        count = 0
+                    clickx, clicky = pyautogui.locateCenterOnScreen(images[0])
+                except TypeError:
+                    print("\r%s not found. Retrying..." %images[0])
+                else:
+                    print("Image found. Stage started")
+                    pyautogui.click(clickx, clicky)
+                    stage_started = 1
+                    mission_started = 0
 
-while True:
-    print("\rChecking for %s" %images[0])
-    while stage_started == 0:
-        try:
-            count += 1
-            if count == 3:
-                pyautogui.click(1700,300)
-                count = 0
-            clickx, clicky = pyautogui.locateCenterOnScreen(images[0])
-        except TypeError:
-            print("\r%s not found. Retrying..." %images[0])
-        else:
-            print("Image found. Stage started")
-            pyautogui.click(clickx, clicky)
-            stage_started = 1
-            mission_started = 0
+            print("\rChecking for %s" %images[1])
+            while stage_started == 1 and mission_started == 0:
+                try:
+                    clickx, clicky = pyautogui.locateCenterOnScreen(images[1])
+                except TypeError:
+                    print("\r%s not found. Retrying..." %images[1])
+                else:
+                    print("Image found. Mission started")
+                    pyautogui.click(clickx, clicky)
+                    stage_started = 0
+                    mission_finished += 1
+                    mission_started = 1
+            print("Mission finished:", mission_finished)
 
-    print("\rChecking for %s" %images[1])
-    while stage_started == 1 and mission_started == 0:
-        try:
-            clickx, clicky = pyautogui.locateCenterOnScreen(images[1])
-        except TypeError:
-            print("\r%s not found. Retrying..." %images[1])
-        else:
-            print("Image found. Mission started")
-            pyautogui.click(clickx, clicky)
-            stage_started = 0
-            mission_finished += 1
-            mission_started = 1
-    print("Mission finished:", mission_finished)
+if __name__ == "__main__":
+    main()
 
-            
 #    for i in images:
 #        try:
 #            print("Checking for %s" %i)
