46e2e3ced77eb3efc065d63469bd2cef35fb19b7
IEatCodesDaily <60133189+IEatCodesDaily@users.noreply.github.com>
2020-08-31 21:07:49 +0700

    Adding description

--- getprocess.py
+++ getprocess.py
@@ -1,17 +1,19 @@
 from win32 import win32gui
 import pywinauto
 
-open_windows = [[]]
 
 #print(active, win32gui.GetWindowText(active))
-def getactive():
-    win32gui.GetForegroundWindow()
+def checkActiveWindow(CorrectWindow):
+    if CorrectWindow == win32gui.GetForegroundWindow():
+        return True
+    else:
+        return False
 
-def getprocess(hwnd, var):
-    if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowTextLength(hwnd) != 0:
-            open_windows.append([hwnd,win32gui.GetWindowText(hwnd)])
-
-
-win32gui.EnumWindows(getprocess, None)
-print(open_windows)
-print(win32gui.GetForegroundWindow())
+def getOpenWindows():
+#Return an array of visible windows PID and Name
+    open_windows = []
+    def enum_handler(hwnd, args):
+        if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowTextLength(hwnd) != 0:
+            open_windows.append([hwnd, win32gui.GetWindowText(hwnd)])
+    win32gui.EnumWindows(enum_handler, None)
+    return open_windows
